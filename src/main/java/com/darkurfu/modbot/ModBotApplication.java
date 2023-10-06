8e0b8273edcb38e01a7b883fe85f719c3fbe273e
SAlexiy <serovalexey@outlook.com>
2023-10-06 23:46:39 +0500

    Initial commit

---
+++ src/main/java/com/darkurfu/modbot/ModBotApplication.java
@@ -0,0 +1,13 @@
+package com.darkurfu.modbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darkurfu/modbot/ModBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkurfu.modbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
